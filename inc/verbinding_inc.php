ecb112d6b568c068c3aa2cd5dff5e8530c27e2cf
ZoomsX1 <yarne@serial-graphics.be>
2016-07-08 04:42:35 +0200

    Added Example config

--- inc/verbinding_inc.php
+++ inc/verbinding_inc.php
@@ -1,9 +1,9 @@
 <?php
 //verbinding met de MySql-database
-$host = "mysql093.webhosting.be";
-$user = "ID131354_webshop";
-$password = "G4NZ9a9K";
-$database = "ID131354_webshop";
+$host = "";
+$user = "";
+$password = "";
+$database = "";
 $link = mysqli_connect($host, $user, $password, $database);
 if (!$link) {
     trigger_error("Fout bij verbinden met database: " . mysqli_connect_error());
